12f1cc69098e4cfd712dfd8def97a75949ac5e79
vinugh <01fe23bca166@kletech.ac.in>
2025-04-13 22:54:26 +0530

    save javafile

---
+++ src/access3.java
@@ -0,0 +1,28 @@
+
+class animal {
+    animal() {
+        System.out.println("Animal constructor called");
+    }
+    animal(int a) {
+        this();
+        System.out.println("Animal constructor with int parameter called: " + a);
+    }
+}
+class dog extends animal {
+    dog() {
+
+        super(20);
+        System.out.println("Dog constructor called");
+    }
+    dog(int a) {
+        this();
+        System.out.println("Dog constructor with int parameter called: " + a);
+    }
+}
+
+public class access3 {
+    public static void main(String[] args) {
+        dog d = new dog(10); 
+    }
+
+}
